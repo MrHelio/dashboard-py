686499615304fc50edc4cfe51993462ced7934a6
Helio <ramon.helio027@gmail.com>
2024-09-20 22:21:28 -0300

    update

--- src/app.py
+++ src/app.py
@@ -3,14 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-st.write("""
-	## DATASUS ##
-	Análise de dados dos anos de 2020 a 2022
-""")
+st.set_page_config(page_title='Dashboard Py', layout='wide')
 
-
-# Título do aplicativo
-st.title('Visualizador de Dados CSV')
+st.title('Educação')
 
 df = pd.read_csv('src/data/part-00000-4a76159e-72db-4184-b120-53352321e01e.c000.csv')
 
+plt
